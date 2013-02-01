f3c5ea7f9406a1760c8a5b44138bdf5a836dec37
Apostolos Pantsiopoulos <apantsiop@gmail.com>
2013-02-01 15:36:44 +0200

    fixes for gricer to work with mongoid 3.x

--- app/controllers/gricer/base_controller.rb
+++ app/controllers/gricer/base_controller.rb
@@ -2,18 +2,18 @@ module Gricer
   # This is the base controller which is used by Gricer's basic statistics controllers
   #
   # You can use this to make your own statistics controllers from a scaffold.
-  class BaseController < ::ApplicationController    
+  class BaseController < ::ApplicationController
     before_filter :guess_from_thru
     helper BaseHelper
-    
+
     layout ::Gricer.config.admin_layout unless ::Gricer.config.admin_layout.nil?
-    
+
     # This action generates a JSON for a process statistics.
     def process_stats
       @items = basic_collection
-      
+
       handle_special_fields
-      
+
       data = {
         alternatives: [
           {
@@ -29,23 +29,23 @@ module Gricer
         step: @stat_step.to_i * 1000,
         data: @items.stat(params[:field], @stat_from, @stat_thru, @stat_step)
       }
-      
+
       if further_details.keys.include? params[:field]
         filters = (params[:filters] || {})
         filters[params[:field]] = '%{self}'
-        
+
         data[:detail_uri] = url_for(action: "process_stats", field: further_details[params[:field]], filters: filters, only_path: true)
       end
-      
+
       render json: data
     end
-    
+
     # This action generates a JSON for a spread statistics.
-    def spread_stats    
+    def spread_stats
       @items = basic_collection.between_dates(@stat_from, @stat_thru)
-      
+
       handle_special_fields
-       
+
       data = {
         alternatives: [
           {
@@ -55,23 +55,23 @@ module Gricer
             type: 'process',
             uri: url_for(action: "process_stats", field: params[:field], filters: params[:filters], only_path: true)
           }
-        ],   
+        ],
         from: @stat_from.to_time.utc.to_i * 1000,
         thru: @stat_thru.to_time.utc.to_i * 1000,
-        total: @items.count(:id),
+        total: @items.count(),
         data: @items.count_by(params[:field])
       }
-      
+
       if further_details.keys.include? params[:field]
         filters = (params[:filters] || {})
         filters[params[:field]] = '%{self}'
-        
+
         data[:detail_uri] = url_for(action: "spread_stats", field: further_details[params[:field]], filters: filters, only_path: true)
       end
-      
+
       render json: data
     end
-    
+
     private
     # @abstract
     # Define which is the basic collection to be processed by the controller.
@@ -79,7 +79,7 @@ module Gricer
     def basic_collection
       raise 'basic_collection must be defined'
     end
-    
+
     # Define how to handle special fields
     def handle_special_fields
       if params[:filters]
@@ -88,25 +88,25 @@ module Gricer
         end
       end
     end
-    
+
     # Define how to handle further details
     def further_details
       {}
     end
-        
+
     # Guess for which time range to display statistics
     def guess_from_thru
       begin
-        @stat_thru = Time.parse(params[:thru]).to_date 
-      rescue       
+        @stat_thru = Time.parse(params[:thru]).to_date
+      rescue
       end
 
       begin
-        @stat_from = Time.parse(params[:from]).to_date 
+        @stat_from = Time.parse(params[:from]).to_date
       rescue
       end
-      
-      if @stat_from.nil? 
+
+      if @stat_from.nil?
         if @stat_thru.nil?
           @stat_thru = Time.now.localtime.to_date - 1.day
         end
@@ -116,23 +116,23 @@ module Gricer
           @stat_thru = @stat_from + 1.week - 1.day
         end
       end
-      
+
       @stat_step = 1.day
-      
+
       duration = @stat_thru - @stat_from
-      
+
       if duration < 90
         @stat_step = 12.hours
       end
-      
+
       if duration < 30
         @stat_step = 6.hour
       end
-      
+
       if duration < 10
         @stat_step = 1.hour
-      end    
-      
+      end
+
       #if @stat_thru - @stat_from > 12.month
       #  @stat_step = 4.week
       #end
--- app/models/gricer/mongoid/session.rb
+++ app/models/gricer/mongoid/session.rb
@@ -9,19 +9,19 @@ module Gricer
         include Mongoid::MapReduce
         include ActiveModel::Session
         include ActiveModel::Statistics
-      
+
         field :requests_count, type: Integer
-      
+
         field :ip_address_hash, type: String
         field :domain, type: String
-      
+
         field :country, type: String
         field :region, type: String
         field :city, type: String
         field :postal_code, type: String
         field :longitude, type: Float
         field :latitude, type: Float
-      
+
         field :javascript, type: Boolean
         field :java, type: Boolean
         field :flash_version, type: String
@@ -34,21 +34,21 @@ module Gricer
         field :screen_depth, type: Integer
         field :requested_locale_major, type: String
         field :requested_locale_minor, type: String
-      
+
         has_many :requests, class_name: 'Gricer::Mongoid::Request', foreign_key: :session_id, order: 'created_at ASC'
         belongs_to :agent, class_name: 'Gricer::Mongoid::Agent', foreign_key: :agent_id, counter_cache: true
         belongs_to :previous_session, class_name: 'Gricer::Mongoid::Session', foreign_key: :previous_session_id
-    
+
         # Filter out anything that is not a Browser or MobileBrowser
         # @return [Mongoid::Criteria]
         def self.browsers
           any_in agent_id: Gricer::Mongoid::Agent.browsers.only(:id).map{|x| x.id}
         end
-        
+
         def self.first_by_id(id)
           where('_id' => id).first
-        end  
-      
+        end
+
         # Get the average duration of sessions in seconds.
         #
         # @return [Float]
@@ -56,15 +56,15 @@ module Gricer
           map = "function() { emit(this.id, new Date(this.updated_at) - new Date(this.created_at)) }"
           reduce = "function (_, xs) { var x = {sum: 0, count: 0}; for (var i=0; i<xs.length; i++) { x.sum += xs[i]; x.count ++ } return x }"
           finalize = "function (_, x) {return x.sum / x.count }"
-        
-          result = map_reduce map, reduce, finalize: finalize
-        
+
+          result = map_reduce(map, reduce).out(:inline => 1).finalize(finalize)
+
           return 0.0 unless result["results"][0]
-        
+
           duration = result["results"][0]["value"] / 1000
-        
+
           duration.nan? ? 0.0 : duration
-        end   
+        end
       end
     end
   end
